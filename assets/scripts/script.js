9f17975df7bd1e3c435c7ce0a619ad5c83fa51fb
Diego Comaschi <comaschi@hotmail.com>
2023-08-23 18:34:00 -0300

    Delete assets directory

--- assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-let navBar= document.querySelector('.header .navbar');
-let menuBtn =document.querySelector('#menu-btn');
-
-
-menuBtn.onclick = () => {
-    menuBtn.classList.toggle('fa-times');
-    navBar.classList.toggle('active');
-}
